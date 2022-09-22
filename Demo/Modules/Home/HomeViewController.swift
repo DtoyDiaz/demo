6f833929ed8f8ee0f1329d12a05ac9160048b9fb
Daniel Diaz <danieldiaz@MacBook-Pro-de-helpdesk.local>
2022-09-22 16:02:31 -0500

    Trying create cells

--- Demo/Modules/Home/HomeViewController.swift
+++ Demo/Modules/Home/HomeViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     var interactor: HomeInteractor?
+    
+    @IBOutlet weak var emailTable: UITableView!
+    
+    let usersTemporal = ["fanny@gmail.com","lili@gmail.com",
+                         "yess@gmail.com","martina@gmail.com",
+                         "jazmin@gmail.com","xiomi@gmail.com",
+                         "cris@gmail.com","vero@gmail.com"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        emailTable.register(UITableViewCell.self, forCellReuseIdentifier: "TableViewCell")
 
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let numberOfrows = usersTemporal.count
+        return numberOfrows
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
+        return cell
+        
     }
     
 }
---
+++ Demo/Modules/Home/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Demo
+//
+//  Created by Daniel Diaz on 22/09/22.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
